9e2b12799917449501dc4a7cc3a11989ef436ba4
Abhishek-raj093 <ss0986825@gmail.com>
2025-01-23 15:26:54 +0530

    first-a

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { CTA, Brand, Navbar } from './src/components';
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+const App = () => {
+  return (
+    <div>
+      <h1>gpt</h1>
+    </div>
+  )
+}
+
+export default App
